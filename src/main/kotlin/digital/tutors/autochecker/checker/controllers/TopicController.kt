597ef927659ff4f892bfbe289cc6d6129604bfba
Daniil Vilyavin <89505105005@mail.ru>
2020-03-28 20:32:59 +0300

    Изменения в контроллерах

--- src/main/kotlin/digital/tutors/autochecker/checker/controllers/TopicController.kt
+++ src/main/kotlin/digital/tutors/autochecker/checker/controllers/TopicController.kt
@@ -1,15 +1,12 @@
 package digital.tutors.autochecker.checker.controllers
 
-import digital.tutors.autochecker.auth.vo.UserCreateRq
-import digital.tutors.autochecker.auth.vo.UserVO
-import digital.tutors.autochecker.checker.entities.Topic
 import digital.tutors.autochecker.checker.services.TopicService
-import digital.tutors.autochecker.checker.vo.TopicVO
+import digital.tutors.autochecker.checker.vo.topic.TopicCreateRq
+import digital.tutors.autochecker.checker.vo.topic.TopicVO
 import digital.tutors.autochecker.core.controller.BaseController
 import digital.tutors.autochecker.core.exception.EntityNotFoundException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
-import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -45,8 +42,8 @@ class TopicController : BaseController() {
     }
 
     @PostMapping("/topic")
-    fun createTopic(@RequestBody topic: Topic): ResponseEntity<TopicVO> = processServiceExceptions {
-        ResponseEntity.ok(topicService.createTopic(topic))
+    fun createTopic(@RequestBody topicCreateRq: TopicCreateRq): ResponseEntity<TopicVO> = processServiceExceptions {
+        ResponseEntity.ok(topicService.createTopic(topicCreateRq))
     }
 
 }
